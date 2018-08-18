fd31f34626709f4e9097903052995d095d9931e3
Your Name <you@example.com>
2018-08-18 19:42:13 +0300

    changed setting for DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,11 +75,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'Asyafirstblog$BlogDB',
+    'USER': 'Asyafirstblog',
+    'PASSWORD': 'barabolia19',
+    'HOST': 'Asyafirstblog.mysql.pythonanywhere-services.com',
+}
 }
 
 
