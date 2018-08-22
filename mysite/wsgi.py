82483945158b985330cf2fc01366a326b939e6c5
Your Name <you@example.com>
2018-08-22 14:19:15 +0300

    changer setting for DM

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,6 +15,13 @@ from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
+if path not in sys.path:
+    sys.path.append(path)
+
+from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
+
 
 #from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
