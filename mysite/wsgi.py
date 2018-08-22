e3e9bed4add77fd8b713378114bf94af737f3427
Your Name <you@example.com>
2018-08-22 14:52:22 +0300

    changet for DM

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,20 +8,14 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
-
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
-
+path = '/home/<your-username>/my-first-blog'  # use your own username here
 if path not in sys.path:
     sys.path.append(path)
 
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
-
-
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
